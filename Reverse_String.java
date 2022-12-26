898e09fc2ec0473e30fd3ac6e484e9697ce18f45
SHIVAM SINGH <91870115+shivam8989@users.noreply.github.com>
2022-12-26 18:05:20 +0530

    Add files via upload
    Reverse a string using Stack

---
+++ Reverse_String.java
@@ -0,0 +1,25 @@
+package Stack;
+import java.util.*;
+public class Reverse_String {
+    public static String  reverseString (String str){
+        Stack <Character> s = new Stack<>();
+        int idx = 0;
+        while(idx<str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result = new StringBuilder("");
+        while(!s.isEmpty()){
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+    public static void main(String args []){
+        String str = "Hello";
+        System.out.println(str);
+        String result = reverseString(str);
+        System.out.println(result);
+
+    }
+}
